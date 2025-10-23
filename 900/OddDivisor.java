e21532efff91a2127989daad914087982c1e738e
Shanmuka-Prasanth-Reddy <sprasanthreddy13@gmail.com>
2025-10-23 14:31:14 +0530

    27/31 in 900 rated

---
+++ 900/OddDivisor.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class OddDivisor {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0) {
+            long n=sc.nextLong();
+            while(n%2==0){
+                n/=2;
+            }
+            System.out.println(n!=1?"Yes":"No");
+        }
+    }
+}
