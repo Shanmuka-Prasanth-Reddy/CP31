2c354288f91893c6dbdb71ccfa77b0de52224e7c
Shanmuka-Prasanth-Reddy <sprasanthreddy13@gmail.com>
2025-10-23 14:20:00 +0530

    26/31 in 900 rated

--- 900/BadBoy.java
+++ 900/BadBoy.java
@@ -3,5 +3,15 @@ import java.util.Scanner;
 public class BadBoy {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0) {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int i=sc.nextInt();
+            int j=sc.nextInt();
+            int x=i-1>=n-i?1:n;
+            int y=j-1>=m-j?1:m;
+            System.out.println(x+" "+y+" "+(x==1?n:1)+" "+(y==1?m:1));
+        }
     }
 }
