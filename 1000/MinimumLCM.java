8d4e657d1197fba10c9a5f481b8d98de212aa5fe
Shanmuka-Prasanth-Reddy <sprasanthreddy13@gmail.com>
2026-01-02 20:49:06 +0530

    9/31 in 1000 rated

---
+++ 1000/MinimumLCM.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MinumumLCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            if (n % 2 == 0) {
+                System.out.println((n / 2) + " " + (n / 2));
+            } 
+            else {
+                long bestDivisor = 1;
+                for (long i = 2; i * i <= n; i++) {
+                    if (n % i == 0) {
+                        bestDivisor = Math.max(bestDivisor, i);
+                        bestDivisor = Math.max(bestDivisor, n / i);
+                    }
+                }
+                System.out.println(bestDivisor + " " + (n - bestDivisor));
+            }
+        }
+    }
+}
