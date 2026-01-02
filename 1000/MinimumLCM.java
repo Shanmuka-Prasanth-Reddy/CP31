08e150053d1b35b02cce634d09d5ea4fc710c36d
Shanmuka-Prasanth-Reddy <sprasanthreddy13@gmail.com>
2026-01-02 20:49:44 +0530

    9/31 in 1000 rated

--- 1000/MinimumLCM.java
+++ 1000/MinimumLCM.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MinumumLCM {
+public class MinimumLCM {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
